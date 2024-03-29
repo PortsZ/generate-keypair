2d234b8fe794925d63d59a00a5e19f6311797743
= <=>
2024-03-29 17:00:46 +0100

    added

--- generate-keypair.ts
+++ generate-keypair.ts
@@ -1,3 +1,14 @@
-import { Keypair } from "@solana/web3.js";
-const keypair = Keypair.generate();
-console.log(`✅ Generated keypair!`);
+import "dotenv/config"
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`
+);
+
+console.log(`The public key is: `, keypair.publicKey.toString());
+
+console.log("\n");
+
+// The public key is:  J3QCoyhDgtvBqwamSizf8dwu9s6iAkdbCCrF2EqgF8VA
